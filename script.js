db92298cef159730f86d134123dcd9a798f82eaa
Jose Mieses <jairo20088@gmail.com>
2019-12-15 23:38:19 -0400

    added scroll and github links

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
-/* const elmt  = document.querySelector('.section-about');
-console.log(elmt.offsetTop);
 
-window.addEventListener('scroll',function(){
-
-    let scroll = window.scrollY;
-
-    if(scroll == 212){
-        console.log('jose mieses');
-    }
-
-}) */
 const menu = document.querySelector('.nav__container');
 const btn = document.querySelector('.nav__menu');
 const close = document.querySelector('#close');
+
+// show nav 
 btn.addEventListener('click',e =>{
     if(e.target.className == 'nav__menu' || e.target.className == 'nav__menu--burger'){
         menu.style.right = 0;
     } 
+});
+
+//  scrool to the element when click
+document.querySelector('.nav__container').addEventListener('click', e =>{
+    e.preventDefault();
+    if(e.target.getAttribute("href")){
+        let element = document.querySelector(e.target.getAttribute("href")).offsetTop;
+    getPosition(element);
+    }
 })
+
+// close nav bar
 close.addEventListener('click', ()=>{
     menu.style.right = '-25rem';
-    console.log('jose')
+});
+
+document.querySelector('.header__btn').addEventListener('click',() =>{
+    getPosition(766);
 })
+
+
+
+function getPosition(element){
+
+    window.scroll({
+        top: element, 
+        left: 0, 
+        behavior: 'smooth'
+      });  
+
+
+}
